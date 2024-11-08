bcd126c181a88fb2cc3630fbee9d6cfdd25c54f3
Martin Reifschneider <mreifschneider@me.com>
2024-11-08 12:05:44 +0100

    Create enemy

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,5 +1,34 @@
 import { Animatable, MovableObject } from "./movableObject.class.js";
 
 export class Chicken extends Animatable(MovableObject) {
+    constructor(x, y, width, height, animationPaths) {        
+        super(animationPaths, x, y, width, height);
+        this.facingRight = true;
+        this.setState('walk');
+    }
 
+    Start() {}
+
+    Update(ctx, deltaTime) {
+        this.move(deltaTime);
+        this.updateAnimation(deltaTime);
+        const frame = this.getCurrentFrame();
+        if(frame) {
+            ctx.save();
+            if(!this.facingRight) this.moveLeft(frame, ctx); 
+            else this.moveRight(frame, ctx);
+            ctx.restore();
+        }
+    }
+
+    moveLeft(frame, ctx) {
+        ctx.translate(this.x + this.width, this.y);
+        ctx.scale(-1, 1);
+        ctx.drawImage(frame, -this.x, this.y, this.width, this.height);
+    }
+
+    moveRight(frame, ctx) {
+        ctx.translate(this.x, this.y);
+        ctx.drawImage(frame, this.x, this.y, this.width, this.height);
+    }
 }
--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
 import { Character } from "../classes/character.class.js";
+import { Chicken } from "../classes/chicken.class.js";
 import { Game } from "../classes/game.class.js";
 
 const game = new Game();
 let pepeImgSrc = '../img/2_character_pepe/2_walk/W-21.png';
-let character;
+let character, chicken;
 const pepeAnimations = {
     idle: { path: 'img/pepe/idle', frameCount: 10 },
     walk: { path: 'img/pepe/walk', frameCount: 6 },
@@ -12,9 +13,17 @@ const pepeAnimations = {
     dead: { path: 'img/pepe/dead', frameCount: 7 }
 }
 
+const chickenAnimations = {
+    walk: { path: 'img/enemy_chicken/walk', frameCount: 3 },
+    dead: { path: 'img/enemy_chicken/dead', frameCount: 1 }
+}
+
 function init() {
     character = new Character(20, 100, 50, 150, pepeAnimations);
+    chicken = new Chicken(200, 160, 30, 30, chickenAnimations);
     game.addGameObject(character);
+    game.addGameObject(chicken);
+
     control();
     game.Start();
 }
